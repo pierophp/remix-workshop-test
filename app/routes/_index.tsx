6f508351ea3ff502ab22c4caa4bb784a6993e9aa
Piero <pierophp@gmail.com>
2024-04-15 14:32:11 -0300

    shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
+import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,8 +15,34 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-blue-500">Bem-vindo ao Workshop!</h1>
-    </div>
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4 text-center">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              The fastest way to deploy your app
+            </h1>
+            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
+              Enter your email to get started with the platform that offers the best developer experience.
+            </p>
+          </div>
+          <div className="w-full max-w-sm space-y-2">
+            <form className="space-y-2">
+              <Input placeholder="Enter your email" required type="email" />
+              <Button className="w-full" type="submit">
+                Sign Up
+              </Button>
+            </form>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              By clicking this button, you agree to the
+              <Link className="underline underline-offset-2" to="https://google.com">
+                Terms & Conditions
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
